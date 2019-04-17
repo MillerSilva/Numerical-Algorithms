a32a8af75a46074be573c1f05d7dd78f14e5b7e6
glozanoa <glozanoa@uni.pe>
2019-04-16 23:30:52 -0500

    Itearciones de Gauss-Seidel

--- sistema_ecuaciones_lineales/iterativos/gauss_seidel.py
+++ sistema_ecuaciones_lineales/iterativos/gauss_seidel.py
@@ -2,13 +2,14 @@ import numpy as np
 import scipy.linalg as sl
 
 def gauss_seidel(A, b, x0, MAX_ITERATIONS):
-    Q = trinf(A)
-    invQ = sl.inv(Q)
-    nrow = np.shape(A)[0]
-    G = np.identity(nrow)-np.dot(invQ, A)
-    if rspectral(G) < 1:
+    if isdiag_domin(A):
+        Q = trinf(A)
+        invQ = sl.inv(Q)
+        nrow = np.shape(A)[0]
+        G = np.identity(nrow)-np.dot(invQ, A)
+        c = np.dot(invQ, b)
         for k in range(MAX_ITERATIONS):
-            x = np.dot(G, x0) + b
+            x = np.dot(G, x0) + c
             x0 = x
         return x
 
@@ -16,7 +17,13 @@ def gauss_seidel(A, b, x0, MAX_ITERATIONS):
         print("El metodo de Gauss-Seidel no es adecuado.")
         return np.zeros_like(x0)
     
-
+def isdiag_domin(A):
+    nrow = np.shape(A)[0]
+    index = np.arange(nrow)
+    for k in range(nrow):
+        if abs(A[k,k]) < sum(np.abs(A[k, index[index != k]])):
+            return False
+    return True
 
 
 
@@ -28,5 +35,3 @@ def trinf(A):
 
     return Q
 
-def rspectral(A):
-    pass
