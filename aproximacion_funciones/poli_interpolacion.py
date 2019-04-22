9fc9016a27e9a3b1c31a822cd53a447e19b97f2e
glozanoa <glozanoa@uni.pe>
2019-04-21 23:32:23 -0500

    agregando grafica del polinomio de interpolacion

--- aproximacion_funciones/poli_interpolacion.py
+++ aproximacion_funciones/poli_interpolacion.py
@@ -1,6 +1,8 @@
 # Implementa el calculo del polinomio de interpolacion de la forma de newton(PFN)
 # Implementa el calculo de los cueficientes del polinomio (PFN)  
 # Implementa la evaluacion del polinomio de interpolacion en algun punto
+# Grafica del polinomio de interpolacion y de los puntos interpolados
+
 
 import numpy as np
 
@@ -11,12 +13,12 @@ def newton(x, y):
 	Calcula los coeficientes del polinomio de interpolacion
 	mediante el metodo de interpolacion de newton
 	"""
-	n = len(x)
+	n = np.shape(x)[0]
 	c = np.array([y[0]])	# coeficientes del polinomio de interpolacion
 	
 	for k in range(1, n):
 		d = x[k] - x[k-1]
-		y = c[k-1]
+		u = c[k-1]
 		for i in range(k-2, -1, -1):
 			u = u*(x[k] - x[i]) + c[i]
 			d *= x[k] - x[i]
@@ -25,11 +27,7 @@ def newton(x, y):
 	return c
 	
 def difdiv(x, y):
-	""""
-	Metodo de interpolacion de diferencias divididas
-	"""
-	
-	nrow = len(y)
+	nrow = np.shape(y)[0]
 	c = np.zeros((nrow, nrow))
 	c[:,0] = y
 	
@@ -40,10 +38,29 @@ def difdiv(x, y):
 	return c[0,:] 	#coeficientes del polinomio de interpolacion
 	
 	
-def eval(c, x, x0): # c: coeficientes, x0: punto a evaluar el polinomio
-	n = len(c)
+def eval_poli(x, c, x0): # c: coeficientes, x0: punto a evaluar el polinomio
+	n = np.shape(x)[0]
 	u  = c[-1]	#u: evaluacion de p en x0
 	for i in range(n-1,-1,-1):
 		u = u*(x0-x[i]) + c[i]
 		
 	return u
+	
+
+# Grafica del polinomio de interpolacion
+
+import matplotlib.pyplot as plt
+
+def grafpoli(x, y, metodo):	# metodo: calcula los coeficientes del polinomio de interolacion este puede ser diferencias divididas o algoritmo de newton
+	px = []
+	c = metodo(x, y)
+	x0 = np.linspace(min(x), max(x), 10000)
+	for a  in x0:
+		px.append(eval_poli(x, c, a))
+
+	plt.plot(x0, px, label="Polinomio de interpolacion")
+	plt.plot(x, y, 'o',label="Puntos interpolados")
+	plt.title("Polinomio de interpolacion de la forma de Newton.")
+	plt.grid()
+	plt.legend()
+	plt.show()
