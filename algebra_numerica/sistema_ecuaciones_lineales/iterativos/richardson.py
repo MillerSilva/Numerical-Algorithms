c26ef11a02060464d9923689e090900f53717769
glozanoa <glozanoa@uni.pe>
2019-04-22 00:29:17 -0500

    agregando la funcion rspectral

--- algebra_numerica/sistema_ecuaciones_lineales/iterativos/richardson.py
+++ algebra_numerica/sistema_ecuaciones_lineales/iterativos/richardson.py
@@ -1,13 +1,28 @@
 # Calcula la solucion del sistema Ax= b, mediante la iteraciones de richardson
 
 import numpy as np
+import scipy.linalg as sl
 
-
-def richardson(A, b, x0, MAX_ITERATIONS=1000):
-    for k in range(MAX_ITERATIONS):
-        r = b-np.dot(A, x0)
-        x = x0 + r
-        x0 = x
+def rspectral(A, v0, f):
+	
+	for k in range(MAX_ITERATIONS):
+		v = np.dot(A, v0)
+    	r = f(v)/f(v0)
+		v0 = v/sl.norm(v)
     
-    return x
+    return r
 
+
+def richardson(A, b, x0, f, MAX_ITERATIONS=1000):
+    nrow = np.shape(A)[0]
+    if rspectral(np.identity(nrow)-A, v0=np.array([1,0,0]).reshape(3,1), f) < 1:
+    
+        for k in range(MAX_ITERATIONS):
+            r = b-np.dot(A, x0)
+            x = x0 + r
+            x0 = x
+    
+        return x
+    else:
+        print("El metodo de richardson no es adecuado.")
+        return x0
